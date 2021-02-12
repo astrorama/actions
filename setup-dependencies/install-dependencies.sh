315bf26daae76b2dd436ca83c9048a54f73e41eb
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2021-02-12 15:00:12 +0100

    Handle numpy specially

--- setup-dependencies/install-dependencies.sh
+++ setup-dependencies/install-dependencies.sh
@@ -6,10 +6,13 @@ source /etc/os-release
 
 # Figure out the python version to use
 PYTHON="python3"
+NUMPY="python3-numpy"
 if [ "$ID" == "fedora" ] && [ "$VERSION_ID" -lt 30 ]; then
   PYTHON="python"
+  NUMPY="python2-numpy"
 elif [ "$ID" == "centos" ] && [ "$VERSION_ID" -lt 8 ]; then
   PYTHON="python"
+  NUMPY="python2-numpy"
 fi
 
 # From the CMakeLists.txt, retrieve the list of dependencies
@@ -22,4 +25,4 @@ yum install -y ${rpm_dev_deps} ${rpm_doc_deps}
 yum install -y cmake make gcc-c++ rpm-build
 
 # Install dependency list
-sed -e "s/\$PYTHON/$PYTHON/" "$1" | xargs yum install -y
+sed -e "s/\$PYTHON/$PYTHON/" "$1" | sed -e "s/\$NUMPY/$NUMPY/" | xargs yum install -y
