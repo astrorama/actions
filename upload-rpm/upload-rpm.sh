b93bf2be7b744517a088670c8efff67c429056ea
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2021-05-26 15:19:16 +0200

    Use system distro/version for remote repo

--- upload-rpm/upload-rpm.sh
+++ upload-rpm/upload-rpm.sh
@@ -8,5 +8,16 @@ else
   REPO="$BRANCH"
 fi
 
+source /etc/os-release
+
+OS_TYPE=${ID}
+OS_ARCH=$(uname -m)
+
+if [[ $OS_TYPE == "centos" ]]; then
+  OS_VERSION=${VERSION_ID}
+else
+  OS_VERSION=$(python3 -c 'import dnf; db = dnf.dnf.Base(); print(db.conf.releasever)')
+fi
+
 jfrog rt u "$1/*.rpm" "rpm/${REPO}/${OS_TYPE}/${OS_VERSION}/${OS_ARCH}/rpms/"
 
