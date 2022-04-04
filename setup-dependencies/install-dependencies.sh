83397aa0adb80ac79557516a6005d90d7bb5cd8b
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2022-04-04 15:47:36 +0200

    Revert "Do not create doc rpm"
    This reverts commit d54ddaca9a71fc9add1e55181e36e123b384a0b9.

--- setup-dependencies/install-dependencies.sh
+++ setup-dependencies/install-dependencies.sh
@@ -18,7 +18,8 @@ fi
 # From the CMakeLists.txt, retrieve the list of dependencies
 cmake_deps=$(grep -oP '^elements_project\(\S+\s+\S+ USE \K(\S+ (\d\.?)+)*' CMakeLists.txt)
 rpm_dev_deps=$(echo ${cmake_deps} | awk '{for(i=1;i<NF;i+=2){print $i "-devel-" $(i+1)}}')
-yum install -y ${rpm_dev_deps}
+rpm_doc_deps=$(echo ${cmake_deps} | awk '{for(i=1;i<NF;i+=2){print $i "-doc-" $(i+1)}}')
+yum install -y ${rpm_dev_deps} ${rpm_doc_deps}
 
 # Common dependencies
 yum install -y cmake make gcc-c++ rpm-build
