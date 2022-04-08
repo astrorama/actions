5abe9864c7c483875fe50932614fb1fbdab0be51
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2022-04-08 09:52:42 +0200

    Do not use -dev for tags

--- elements-build-rpm/build-rpm.sh
+++ elements-build-rpm/build-rpm.sh
@@ -14,16 +14,15 @@ source /etc/os-release
 
 # Common flags
 CMAKEFLAGS="-DINSTALL_DOC=ON -DUSE_SPHINX_APIDOC=OFF -DCMAKE_INSTALL_PREFIX=/usr -DINSTALL_TESTS=OFF -DRPM_NO_CHECK=OFF"
-if [[ "${BRANCH}" != "master" && -z "${RELEASE}" ]]; then
+
+if [[ ! -z "${RELEASE}" ]]; then
+  CMAKEFLAGSS="${CMAKEFLAGS} -DCPACK_PACKAGE_RELEASE=${RELEASE}"
+elif [[ "${BRANCH}" != "master" && "${GITHUB_REF_TYPE}" != "tag" ]]; then
   CMAKEFLAGS="${CMAKEFLAGS} -DCPACK_PACKAGE_RELEASE=dev"
 fi
 
 if [[ ("$ID" == "fedora" && "$VERSION_ID" -ge 30) || ("$ID" == "centos" && "$VERSION_ID" -ge 8) ]]; then
   CMAKEFLAGS="${CMAKEFLAGS} -DPYTHON_EXPLICIT_VERSION=3"
-fi
-
-if [[ ! -z "${RELEASE}" ]]; then
-  CMAKEFLAGSS="${CMAKEFLAGS} -DCPACK_PACKAGE_RELEASE=${RELEASE}"
 fi
 
 # Build
