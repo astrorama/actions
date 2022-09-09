dfcd866e17eb633cbd5592aef1f8c74674a2fb00
Alejandro Alvarez Ayllon <a.alvarezayllon@gmail.com>
2022-09-09 12:05:36 +0200

    Override boost on centos7

--- setup-dependencies/install-dependencies.sh
+++ setup-dependencies/install-dependencies.sh
@@ -5,14 +5,16 @@ set -ex
 source /etc/os-release
 
 # Figure out the python version to use
-PYTHON="python3"
-NUMPY="python3-numpy"
+export PYTHON="python3"
+export NUMPY="python3-numpy"
+export BOOST="boost"
 if [ "$ID" == "fedora" ] && [ "$VERSION_ID" -lt 30 ]; then
   PYTHON="python"
   NUMPY="python2-numpy"
 elif [ "$ID" == "centos" ] && [ "$VERSION_ID" -lt 8 ]; then
   PYTHON="python"
   NUMPY="python2-numpy"
+  BOOST="boost169"
 fi
 
 # From the CMakeLists.txt, retrieve the list of dependencies
@@ -25,4 +27,4 @@ yum install -y ${rpm_dev_deps} ${rpm_doc_deps}
 yum install -y cmake make gcc-c++ rpm-build
 
 # Install dependency list
-sed -e "s/\$PYTHON/$PYTHON/" "$1" | sed -e "s/\$NUMPY/$NUMPY/" | xargs yum install -y
+envsubst < "$1" | xargs yum install -y
