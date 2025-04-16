6122133fb5351e3a68012b9c312592c7d6bded7e
Marc Schefer <31961290+marcschefer@users.noreply.github.com>
2025-04-16 15:58:36 +0200

    And fedora 40 wants gawk not awk

--- setup-dependencies/install-dependencies.sh
+++ setup-dependencies/install-dependencies.sh
@@ -18,7 +18,7 @@ elif [[ "$ID" == "centos"  && "$VERSION_ID" -lt 8 ]]; then
   BOOST="boost169"
 fi
 
-yum install -y awk
+yum install -y gawk
 
 # From the CMakeLists.txt, retrieve the list of dependencies
 cmake_deps=$(grep -oP '^elements_project\(\S+\s+\S+ USE \K(\S+ (\d\.?)+)*' CMakeLists.txt || true)
